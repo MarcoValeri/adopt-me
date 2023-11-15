36fdeac4ebfd7949960e05bd8283f56e1b17fffc
Marco Valeri <marco.valeri@cherrythinking.com>
2023-11-15 17:18:06 +0000

    Add searchParam component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { createRoot } from 'react-dom';
 
-import Pet from './components/Pet/Pet';
+import SearchParams from './components/Pet/SearchParams/SearchParams';
 
 const App = () => {
     return (
         <div>
             <h1>Adopt Me!</h1>
-            <Pet name="Luna" animal="dog" breed="Havanese" />
-            <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-            <Pet name="Doink" animal="cat" breed="Mixed" />
+            <SearchParams />
         </div>
     )
 };
